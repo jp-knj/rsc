ab60725f26a561bde0fe6497c8abb25bf10f56e8
jp-knj <tomita.kenji1996@gmail.com>
2024-06-22 23:41:59 +0900

    setup rsc-vite

--- rsc-vite/src/ViteRuntime.ts
+++
@@ -1,7 +0,0 @@
-import { createServer, createViteRuntime } from "vite";
-
-export const server = await createServer({
-  configFile: "./vite.config.ts",
-});
-
-export const runtime = await createViteRuntime(server);
