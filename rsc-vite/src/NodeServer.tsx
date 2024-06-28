5ecdc9f24ccf73b8b41bcfd3656aff4b9ab4ed3b
jp-knj <tomita.kenji1996@gmail.com>
2024-06-28 21:33:10 +0900

    creating a client runtime

--- rsc-vite/src/NodeServer.tsx
+++ rsc-vite/src/NodeServer.tsx
@@ -1,8 +1,17 @@
 import { serve } from "@hono/node-server";
 import { Hono } from "hono";
-import { createServer } from "node:http";
+import { createServer as createHttpServer, IncomingMessage, ServerResponse } from 'node:http';
+
+import * as Vite from "./ViteRuntime.ts";
+import { ServerOptions } from 'http';
 
 const app = new Hono();
+
+const createServer = ((options, listener) => {
+  return createHttpServer(options as ServerOptions<typeof IncomingMessage, typeof ServerResponse> , (req, res) =>
+    Vite.server.middlewares.handle(req, res, () => listener?.(req, res))
+  );
+}) as typeof createHttpServer;
 
 const server = serve({
   createServer,
